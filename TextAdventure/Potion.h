e83d6b7bb2dceecb3284f60bffb405a013e39321
AlteredDev <rudhy.travail@gmail.com>
2020-11-27 09:37:57 +0100

    Des trucs ajoutés

---
+++ TextAdventure/Potion.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Potion
+{
+private:
+	string name;
+	int timeTurn;
+	bool isDamageBoost;
+	float damageBoost;
+	bool isHeal;
+	float heal;
+	bool isSpeed;
+	float speed;
+
+public:
+	Potion();
+	Potion(string n, int tt, bool idb, float db, bool ih, float h, bool is, float s);
+
+	string getName();
+	int getTimeTurn();
+	bool getIsDamageBoost();
+	float getDamageBoost();
+	bool getIsHeal();
+	float getHeal();
+	bool getIsSpeed();
+	float getSpeed();
+};
+
--- TextAdventure/Source.cpp
+++ TextAdventure/Source.cpp
@@ -11,26 +11,31 @@ int RangeIntInput(int min, int max) {
 	return input;
 }
 
-void showGroupInv(Inventory inv, vector<Character*> group) {
+void showGroupInv(Inventory inv, vector<Character> group) {
 	system("cls");
 
 	cout << "\nWeapon :\n\n";
 	for (int i = 0; i < inv.getWeaponTab().size(); i++) {
-		if (inv.getWeaponTab()[i] == NULL) {break;}
+		if (inv.getWeaponTab()[i] == nullptr) { break; }
 		cout << "\t" << inv.getWeaponTab()[i]->GetName() << endl;
 	}
 
 	cout << "\n\nItems :\n\n";
+	for (int i = 0; i < inv.getItemTab()->getPotionTab().size(); i++) {
+		if (inv.getItemTab()->getPotionTab()[i] == nullptr) { continue; }
+		cout << "\t" << inv.getItemTab()->getPotionTab()[i]->getName() << endl;
+	}
 
 	cout << "\n\nEquipment :\n\n";
 	for (int i = 0; i < group.size(); i++) {
-		if (group[i]->getActualWeapon() != NULL) {
-			cout << "\t" << group[i]->getFirstName() << " " << group[i]->getLastName() << " is equipped with a " << group[i]->getActualWeapon()->GetName() << endl;
+		if (group[i].getActualWeapon() != nullptr) {
+			cout << "\t" << group[i].getFirstName() << " " << group[i].getLastName() << " is equipped with a " << group[i].getActualWeapon()->GetName() << endl;
 		}
 		else {
-			cout << "\t" << group[i]->getFirstName() << " " << group[i]->getLastName() << " is unequipped" << endl;
+			cout << "\t" << group[i].getFirstName() << " " << group[i].getLastName() << " is unequipped" << endl;
 		}
 	}
+	cout << endl; system("pause");
 }
 
 int main() {
@@ -41,10 +46,16 @@ int main() {
 	Weapon w4("Bad Spear", 15, 0, 5);
 	Weapon w5("Spiked Shield", 5, 7, 35);
 
-	vector<Character*> group;
-	Inventory groupInv;
+	Potion po1("Speed Potion", 2, false, 0, false, 0, true, 2);
+	Potion po2("Health Potion", 1, false, 0, true, 5, false, 0);
+	Potion po3("Strength Potion", 3, true, 2, false, 0, false, 0);
 
-	vector<Character*> ennemies;
+	vector<Character> group;
+	Inventory groupInv;
+	Item groupItem;
+	groupInv.setItemTab(&groupItem); //bind les Item à l'inv de group
+
+	vector<Character> ennemies;
 
 	int intInput;
 
@@ -60,35 +71,40 @@ int main() {
 	for (int i = 0; i < intInput; i++) {
 		system("cls");
 		Character newCharacter;
-		group.push_back(&newCharacter); //inserting the character
-		group[i]->createCharacter(); //creation du perso
-		group[i]->setInvChar(&groupInv); //bind l'inv du perso à celui du group
+		group.push_back(newCharacter); //inserting the character
+		group[i].createCharacter(); //creation du perso
+		group[i].setInvChar(&groupInv); //bind l'inv du perso à celui du group
 	}
-	groupInv.setInvSize(group.size() * 4); //set inv size
+	groupInv.setInvSize(group.size() * 4); //set inv size (only for the weapons)
 	system("cls");
 	for (int i = 0; i < group.size(); i++) {
-		if (group[i]->getClassName() == "Mage") {
+		if (group[i].getClassName() == "Mage") {
 			groupInv.addWeapon(&w3);
 			continue;
 		}
-		else if (group[i]->getClassName() == "Paladin") {
+		else if (group[i].getClassName() == "Paladin") {
 			groupInv.addWeapon(&w4);
 			continue;
 		}
-		else if (group[i]->getClassName() == "Warrior") {
+		else if (group[i].getClassName() == "Warrior") {
 			groupInv.addWeapon(&w2);
 			continue;
 		}
 	}
 
 	showGroupInv(groupInv, group);
+	groupInv.getItemTab()->addPotion(&po1);
+	groupInv.getItemTab()->addPotion(&po2);
+	groupInv.getItemTab()->addPotion(&po3);
 
 	for (int i = 0; i < group.size(); i++) {
-		group[i]->equipWeapon(&groupInv);
+		group[i].equipWeapon(&groupInv);
 	}
 
 
 	showGroupInv(groupInv, group);
+
+
 
 
 	/*Spell s;
